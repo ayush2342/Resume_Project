319206d123d4e25bd2c35c9e032764aa95b4829c
ayush2342 <ayushsharma2342@gmail.com>
2023-02-05 12:53:56 +0530

    ScrollTransition Added on Navbar links

---
+++ script.js
@@ -0,0 +1,40 @@
+"use script";
+
+var anchorTags=document.querySelectorAll(".nav-menu a");
+
+var currPosition=0;
+var destiPosition;
+
+for(var i=0;i<anchorTags.length;i++)
+{
+
+anchorTags[i].addEventListener("click",function Scroll(event){
+event.preventDefault();
+var targetSection = this.innerHTML.trim().toLowerCase();
+
+
+var targetElement=document.getElementById(targetSection);
+
+destiPosition=targetElement.getBoundingClientRect().y;
+
+var ScrollInterval= setInterval(function IntervalHandler()
+{
+   
+    if(currPosition>=destiPosition)
+    {
+        currPosition=0;
+        clearInterval(ScrollInterval);
+    }
+   window.scrollBy(0,50);
+   currPosition+=50;
+   console.log(currPosition);
+   console.log(destiPosition);
+
+},50);
+
+})
+}
+
+
+
+
